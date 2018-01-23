deb5487f934eb58d53d208df9d86b0cd1ec7d7de
rtisma <rtisma@gmail.com>
2018-01-23 18:29:27 -0500

    - added postgres connection check - added consol logging and logging to file - check for arguments defined and if ini file exists

--- postgres_db_s3_backup.py
+++ postgres_db_s3_backup.py
@@ -7,9 +7,27 @@ import datetime
 import configparser
 import sys
 import logging
+import psycopg2
 
-logging.basicConfig(level=logging.INFO)
-log = logging.getLogger("db_backup")
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                    datefmt='%m-%d %H:%M',
+                    filename='./postgres-db-s3-backup.log',
+                    filemode='w')
+
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+console.setFormatter(formatter)
+
+log_main = logging.getLogger("main")
+log_db_dump = logging.getLogger("DB_Dump")
+log_s3 = logging.getLogger("S3Client")
+
+log_main.addHandler(console)
+log_db_dump.addHandler(console)
+log_s3.addHandler(console)
+
 
 def run_command(command):
     out = subprocess.run(command,
@@ -18,6 +36,7 @@ def run_command(command):
                           stderr=subprocess.PIPE,
                           check=True)
     out.check_returncode()
+    check_state(out.stderr is None or len(out.stderr) == 0, "The following error occurred: {}", out.stderr)
     return out
 
 
@@ -40,6 +59,19 @@ class DB_Dump(object):
         self.password = password
         self.output_dir = output_dir
         self.dump_filename_prefix = dump_filename_prefix
+
+    def _check_connection(self, dbname):
+        log_db_dump.info("Checking database connection...")
+        try:
+            conn = psycopg2.connect(dbname=dbname, user=self.user,
+                                    host=self.host, port=self.port, password=self.password)
+            conn.close()
+        except Exception as e:
+            msg = "Unable to connect to postgres for host={}, dbname={} and user={} for Reason: {}".format(
+                self.host, dbname, self.user, e)
+            log_db_dump.error(msg)
+            raise
+
 
     @classmethod
     def _prepare_output_file(cls, output_file):
@@ -64,9 +96,11 @@ class DB_Dump(object):
         dump_cmd = "PGPASSWORD={} pg_dump -h {}  -p {} -U {} {}".format(
             self.password, self.host, self.port, self.user, dbname)
         compress_cmd = "gzip > {}".format(output_file)
+        self._check_connection(dbname)
         process_output = run_command(dump_cmd+" | "+compress_cmd)
         check_file(output_file)
         return output_file
+
 
 class S3Client:
     def __init__(self, endpoint_url, access_key, secret_key,
@@ -78,10 +112,10 @@ class S3Client:
 
         # state
         self.__client = self.__connect()
-        # self.__buckets = map(lambda x: x['name'],self.__client.list_buckets()['Buckets'])
         self.__buckets = S3Client.__get_bucket_names(self.__client)
 
     def __connect(self):
+        log_s3.debug("Connecting to S3 client...")
         return boto3.client('s3',
                             aws_access_key_id=self.__access_key,
                             aws_secret_access_key=self.__secret_key,
@@ -90,6 +124,7 @@ class S3Client:
 
     @classmethod
     def __get_bucket_names(cls, client):
+        log_s3.debug("Retrieving list of all buckets...")
         out = []
         for entry in client.list_buckets()['Buckets']:
             out.append(entry['Name'])
@@ -111,7 +146,6 @@ class S3Client:
         self.check_bucket_existence()
         base_filename = os.path.basename(local_filename)
         key = base_filename # self.__generate_key(base_filename)
-        print()
         self.__client.upload_file(local_filename,
                                   self.data_bucket,
                                   key)
@@ -124,6 +158,7 @@ class S3Client:
 
     def setup_bucket(self, name):
         if not self.is_bucket_exists(name):
+            log_s3.warning("The bucket '{}' does not exist. Creating it ...".format(name))
             self.__client.create_bucket(Bucket=name)
 
     def is_bucket_exists(self, name):
@@ -131,7 +166,13 @@ class S3Client:
 
 
 def main():
+    if len(sys.argv) < 2:
+        raise Exception("Missing the input config ini filename")
+    elif len(sys.argv) > 2:
+        raise Exception("Too many arguments")
+
     ini_file = sys.argv[1]
+    check_file(ini_file)
     config = configparser.ConfigParser()
     config.read(ini_file)
     cfg = config.defaults()
@@ -147,20 +188,20 @@ def main():
     backup_file_prefix = cfg['backup_file_prefix']
     backup_output_dir = cfg['backup_output_dir']
     dumper = DB_Dump(db_user, db_password, host=db_host, port=db_port, dump_filename_prefix=backup_file_prefix, output_dir=backup_output_dir)
-    log.info("Dumping '{}' postgres database...".format(db_name))
+    log_main.info("Dumping '{}' postgres database...".format(db_name))
     output_filename = dumper.dump_db(db_name)
-    log.info("\t- successfully dumped to file '{}'".format(output_filename))
+    log_main.info("\t- successfully dumped to file '{}'".format(output_filename))
 
     client = S3Client(s3_endpoint_url, s3_access_key, s3_secret_key, s3_data_bucket)
-    log.info("Setting up buckets...")
+    log_main.info("Setting up buckets...")
     client.setup()
-    log.info("Uploading file '{}' to s3 at endpoint '{}'...".format(output_filename, s3_endpoint_url))
+    log_main.info("Uploading file '{}' to s3 at endpoint '{}'...".format(output_filename, s3_endpoint_url))
     client.upload(output_filename)
-    log.info("\t- successfully uploaded file '{}' to bucket '{}'".format(output_filename, s3_data_bucket))
+    log_main.info("\t- successfully uploaded file '{}' to bucket '{}'".format(output_filename, s3_data_bucket))
 
     if os.path.exists(output_filename):
         os.remove(output_filename)
-        log.info("Removed file '{}'".format(output_filename))
+        log_main.info("Removed file '{}'".format(output_filename))
 
 
 if __name__ == '__main__':
